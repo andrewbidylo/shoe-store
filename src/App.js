b1de282cee09b44d4647fd67db19e224a1d7959a
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2021-12-21 11:07:40 -0800

    Add cards

--- src/App.js
+++ src/App.js
@@ -6,24 +6,84 @@ function App() {
           <img className= 'logo' src='img/logo.png' alt='logo-sneakers'/>
           <div className='headerInfo'>
             <h3 className='text-uppercase'>React Sneakers</h3>
-            <p>Sneakers store</p>
+            <p className='opacity-5'>Sneakers store</p>
           </div>
         </div>
         <ul className='d-flex'>
           <li className='mr-30'>
-            <img src='/img/cart.svg' />
+            <img src='/img/cart.svg' alt=''/>
             <span>
               $100
             </span>
           </li>
           <li>
-          <img src='/img/user.svg' />
+          <img src='/img/user.svg' alt=''/>
           </li>
         </ul>
       </header>
-      <section className='p-40'>
-        <h1>All Sneakers</h1>
-        ......
+      <section className='content p-40'>
+        <h1 className='mb-40'>All Sneakers</h1>
+        <div className='allSneackers d-flex justify-between'>
+        <div className='card'>
+       <img src='/img/sneakers/1.png' alt='plus' className='sneakers-pic'></img>
+       <h5>Mens shoes</h5>
+       <div className='d-flex justify-between align-center'>
+       <div className='d-flex flex-column'>
+         <span>Price:</span>
+          <b> $100</b>
+         </div>
+         <button className='button '>
+           <img src='/img/plus.svg' alt='plus'></img>
+         </button>
+       </div>
+       </div>
+
+
+
+       <div className='card'>
+       <img src='/img/sneakers/1.png' alt='plus' className='sneakers-pic'></img>
+       <h5>Mens shoes</h5>
+       <div className='d-flex justify-between align-center'>
+       <div className='d-flex flex-column'>
+         <span>Price:</span>
+          <b> $100</b>
+         </div>
+         <button className='button '>
+           <img src='/img/plus.svg' alt='plus'></img>
+         </button>
+       </div>
+       </div>
+
+
+       <div className='card'>
+       <img src='/img/sneakers/1.png' alt='plus' className='sneakers-pic'></img>
+       <h5>Mens shoes</h5>
+       <div className='d-flex justify-between align-center'>
+       <div className='d-flex flex-column'>
+         <span>Price:</span>
+          <b> $100</b>
+         </div>
+         <button className='button '>
+           <img src='/img/plus.svg' alt='plus'></img>
+         </button>
+       </div>
+       </div>
+
+       <div className='card'>
+       <img src='/img/sneakers/1.png' alt='plus' className='sneakers-pic'></img>
+       <h5>Mens shoes</h5>
+       <div className='d-flex justify-between align-center'>
+       <div className='d-flex flex-column'>
+         <span>Price:</span>
+          <b> $100</b>
+         </div>
+         <button className='button '>
+           <img src='/img/plus.svg' alt='plus'></img>
+         </button>
+       </div>
+       </div>
+        </div>
+      
       </section>
     </div>
   );
