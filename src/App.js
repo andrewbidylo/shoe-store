52cc7121b50b59422f2861896fa0645b9e1259e7
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2021-12-20 17:44:23 -0800

    add style to header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 function App() {
   return (
-    <div className="wrapper">
-      <header className="App-header">
-        <div className="headerLeft">
-          <img src='img/logo.png' alt='logo-sneakers'/>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="headerLeft d-flex align-center">
+          <img className= 'logo' src='img/logo.png' alt='logo-sneakers'/>
           <div className='headerInfo'>
-            <h3>React Sneakers</h3>
+            <h3 className='text-uppercase'>React Sneakers</h3>
             <p>Sneakers store</p>
           </div>
         </div>
-        <ul className='headerRight'>
-          <li>
-            <svg />
+        <ul className='d-flex'>
+          <li className='mr-30'>
+            <img src='/img/cart.svg' />
             <span>
               $100
             </span>
           </li>
           <li>
-            <svg />
+          <img src='/img/user.svg' />
           </li>
         </ul>
       </header>
-      <section className='content'>
+      <section className='p-40'>
         <h1>All Sneakers</h1>
         ......
       </section>
