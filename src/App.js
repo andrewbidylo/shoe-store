1baa03a5b5abb1d088aad3dd11b4ec19ea043109
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-04-06 18:52:52 -0700

    add react-content-loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 import React, { useEffect } from 'react';
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,46 +1,79 @@
 import styles from './Card.module.scss'
-import React from 'react'
+import React, { useState } from 'react'
+import ContentLoader from 'react-content-loader';
+// import AppContext from '../../context';
+function Card({
+  id,
+  title,
+  price,
+  imageURL,
+  onClickAdd,
+  onClickFavorite,
+  favorited = false,
+  added = false,
+  loading = false
+}) {
 
-
-function Card({ id, title, price, imageURL, onClickAdd, onClickFavorite, favorited = false, added = false}) {
-
-  const [isAdded, setIsAdded] = React.useState(added)
-  const [isFavorite, setIsFavorite] = React.useState(favorited)
-
+  const [isAdded, setIsAdded] = useState(added)
+  const [isFavorite, setIsFavorite] = useState(favorited)
+  // const { isItemAdded } = React.useContext(AppContext);
   // Pass {title, price, imageURL} from the selected item to the Card. 
   // Change the Cart's status.  
   const onClickPlus = () => {
-    onClickAdd({ id, title, price, imageURL})
+    onClickAdd({ id, title, price, imageURL })
     setIsAdded(!isAdded)
   }
 
   const onClickFavoriteButton = () => {
-    onClickFavorite({ id, title, price, imageURL})
+    onClickFavorite({ id, title, price, imageURL })
     setIsFavorite(!isFavorite)
   }
 
 
   return (
-    <div className={styles.card} >
-      <div className={styles.favorite}>
-        <img src={isFavorite ? "./img/liked.svg" : '/img/unliked.svg'}  alt='unliked' onClick={onClickFavoriteButton} />
-      </div>
-      <img src={imageURL} alt='plus' className={styles.sneakersPic}></img>
-      <h5>{title}</h5>
-      <div className='d-flex justify-between align-center'>
-        <div className='d-flex flex-column'>
-          <span>Price:</span>
-          <b>${price}</b>
-        </div>
-        <img onClick={onClickPlus}
-          className={styles.addItem}
-          src={isAdded ? "./img/btn-checked.svg" : "./img/btn-plus.svg"}
-          alt='plus'/>
-      </div>
+    <div className={styles.card}>
+      {loading ? (
+        <ContentLoader
+          speed={2}
+          width={155}
+          height={250}
+          viewBox="0 0 155 265"
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb">
+          <rect x="1" y="0" rx="10" ry="10" width="155" height="155" />
+          <rect x="0" y="167" rx="5" ry="5" width="155" height="15" />
+          <rect x="0" y="187" rx="5" ry="5" width="100" height="15" />
+          <rect x="1" y="234" rx="5" ry="5" width="80" height="25" />
+          <rect x="124" y="230" rx="10" ry="10" width="32" height="32" />
+        </ContentLoader>
+      ) : (
+        <>
+          {onClickFavorite && (
+            <div className={styles.favorite} onClick={onClickFavoriteButton}>
+              <img src={isFavorite ? 'img/liked.svg' : 'img/unliked.svg'} alt="Unliked" />
+            </div>
+          )}
+          <img width="100%" height={180} src={imageURL} alt="Sneakers" />
+          <h5>{title}</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Price:</span>
+              <b>${price}</b>
+            </div>
+            {onClickAdd && (
+              <img onClick={onClickPlus}
+                className={styles.addItem}
+                src={isAdded ? "./img/btn-checked.svg" : "./img/btn-plus.svg"}
+                alt='plus' />
+
+              // src={isItemAdded(id) ? 'img/btn-checked.svg' : 'img/btn-plus.svg'}
+
+            )}
+          </div>
+        </>
+      )}
     </div>
   )
 }
 
 export default Card
-
-
